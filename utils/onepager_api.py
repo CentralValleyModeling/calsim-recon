a5d9ec0bf5c887ab1b57fad415afba6ac85ca8f4
Auhona Zaki <Auhona.Zaki@water.ca.gov>
2024-09-11 14:51:11 -0700

    changed csv filename to dv_data.csv coming from the data folder

--- utils/onepager_api.py
+++ utils/onepager_api.py
@@ -4,23 +4,30 @@ import logging
 import pandas as pd
 import pandss as pdss
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
 # Inputting file
 logging.basicConfig(level=logging.INFO)
 
 
 def cfs_to_taf(df: pd.DataFrame, col: str) -> pd.DataFrame:
+    df = df.copy()
     periods = pd.PeriodIndex(data=df.index, freq="1M")
     days = periods.days_in_month
 
-    df[col] = df[col] * days # CFS/sec to CFS/Day
-    df[col] = df[col] * (24 * 60 * 60) # CFS/Day to CFS/sec
-    df[col] = df[col] / 43_560 # CFS/sec to AF/sec
-    df[col] = df[col] / 1000 # AF/sec to TAF/sec
+    df[col] = df[col] * days  # CFS/sec to CFS/Day
+    df[col] = df[col] * (24 * 60 * 60)  # CFS/Day to CFS/sec
+    df[col] = df[col] / 43_560  # CFS/sec to AF/sec
+    df[col] = df[col] / 1000  # AF/sec to TAF/sec
     return df
 
-def table_a(dss_obj: pdss.DSS, path_string_swp: str, start_date: pd.Timestamp, end_date: pd.Timestamp)->pd.DataFrame:
+
+def table_a(
+    dss_obj: pdss.DSS,
+    path_string_swp: str,
+    start_date: pd.Timestamp,
+    end_date: pd.Timestamp,
+) -> pd.DataFrame:
     df = None
     path_swp = pdss.DatasetPath.from_str(path_string_swp)
 
@@ -37,18 +44,20 @@ def table_a(dss_obj: pdss.DSS, path_string_swp: str, start_date: pd.Timestamp, e
         # Convert the unit if necessary
         if rts.units.lower() == "cfs":
             df = cfs_to_taf(df, col="VALUE")
-    # TODO: implement 12/9 multiplication
-        
+        # TODO: implement 12/9 multiplication
+
         # Create a mask for the data within the desired range of dates
         mask = (df.index >= start_date) & (df.index <= end_date)
 
         # Filter out any data that is not within the range indicated by the mask
-        df = df.loc[mask] 
+        df = df.loc[mask]
 
     return df
 
 
-def period_avg(calendar_year_df: pd.DataFrame, start_year: int = None, end_year: int = None)-> tuple:
+def period_avg(
+    calendar_year_df: pd.DataFrame, start_year: int = None, end_year: int = None
+) -> tuple:
     # Filter out unnecessary years
     if start_year is not None and end_year is not None:
         start = pd.to_datetime(str(start_year) + "-12-31")
@@ -58,46 +67,51 @@ def period_avg(calendar_year_df: pd.DataFrame, start_year: int = None, end_year:
         calendar_year_df = calendar_year_df.loc[mask]
 
     # Calculate the annual average
-    calendar_year_sum_avg = int(calendar_year_df['VALUE'].mean())
+    calendar_year_sum_avg = int(calendar_year_df["VALUE"].mean())
     calendar_year_sum_avg_percent = int((calendar_year_sum_avg / 4113) * 100)
     return (calendar_year_sum_avg, calendar_year_sum_avg_percent)
 
 
-def rank_and_pick_year(df: pd.DataFrame)-> pd.DataFrame:
+def rank_and_pick_year(df: pd.DataFrame) -> pd.DataFrame:
     df1 = df.copy(deep=True)
     df1 = df1.sort_values(by=["VALUE"], ascending=False)
     return df1
 
 
-def wettest_year(df: pd.DataFrame, rank: int)-> tuple:
+def wettest_year(df: pd.DataFrame, rank: int) -> tuple:
     row = df.iloc[rank]
-    val = int(row['VALUE'])
+    val = int(row["VALUE"])
     percent = int((val / 4113) * 100)
     year = row.name.year
 
     return (year, val, percent)
 
 
-def table_a_from_csv(df: pd.DataFrame, path_string_swp: str, start_date: pd.Timestamp, end_date: pd.Timestamp)->pd.DataFrame:
-    #df1 = df.loc[df["Scenario"] == "AdjHist"]
-    #df2 = df1["SWP_TA_TOTAL"]
-    #return df2
+def table_a_from_csv(
+    df: pd.DataFrame,
+    path_string_swp: str,
+    start_date: pd.Timestamp,
+    end_date: pd.Timestamp,
+) -> pd.DataFrame:
+    # df1 = df.loc[df["Scenario"] == "AdjHist"]
+    # df2 = df1["SWP_TA_TOTAL"]
+    # return df2
 
     # Check the unit and convert if necessary
-    #if rts.units.lower() == "cfs":
+    # if rts.units.lower() == "cfs":
     #    df = cfs_to_taf(df, col=path_string_swp)
-        
+
     # Create a mask for the data within the desired range of dates
     mask = (df.index >= start_date) & (df.index <= end_date)
 
     # Filter out any data that is not within the range indicated by the mask
     df = df.loc[mask]
-    
+
     # We don't know the unit so just assuming that the conversion is needed
     df = cfs_to_taf(df, col="VALUE")
 
     # Rename the column containing the data to VALUE
-    #df = df.rename(columns={path_string_swp : 'VALUE'})
+    # df = df.rename(columns={path_string_swp : 'VALUE'})
 
     return df
 
@@ -107,23 +121,18 @@ def read_run_to_structure_csv(df: pd.DataFrame) -> dict:
     table = {}
 
     # DSS key path for timeseries
-    path_swp_list = [
-        "SWP_TA_TOTAL", 
-        "SWP_CO_TOTAL", 
-        "SWP_TA_FEATH", 
-        "SWP_CO_FEATH"
-        ]
-    
+    path_swp_list = ["SWP_TA_TOTAL", "SWP_CO_TOTAL", "SWP_TA_FEATH", "SWP_CO_FEATH"]
+
     # date range we are interested in
     start = pd.to_datetime("1921-10-01")
     end = pd.to_datetime("2021-09-30")
 
     frames: list[pd.DataFrame] = []
-    
+
     for path_string_swp in path_swp_list:
         # Create a new dataframe with values only
         df1 = df[[path_string_swp]]
-        df1 = df1.rename(columns={path_string_swp : 'VALUE'})
+        df1 = df1.rename(columns={path_string_swp: "VALUE"})
 
         # Get the data frame for the given path
         df2 = table_a_from_csv(df1, path_string_swp, start, end)
@@ -135,12 +144,12 @@ def read_run_to_structure_csv(df: pd.DataFrame) -> dict:
     df_A = frames[0] + frames[1]
     df_A = df_A - frames[2]
     df_A = df_A - frames[3]
-    #print(df_A)
+    # print(df_A)
 
     # Convert df from monthly to yearly
     calendar_year_df = df_A.resample(pd.offsets.YearEnd()).sum()
     print(calendar_year_df.head())
-    
+
     # Column 1. long term average
     table["Long-term Average"] = period_avg(calendar_year_df)
 
@@ -171,7 +180,7 @@ def read_run_to_structure_csv(df: pd.DataFrame) -> dict:
 
     # Column 8: Single Wettest Year (3rd most)
     wettest_yr, wettest_val, wettest_perc = wettest_year(ranked_df, 2)
-    #wettest_key = f"Single Wet Year ({wettest_yr})"
+    # wettest_key = f"Single Wet Year ({wettest_yr})"
     wettest_key = "Single Wet Year (1938)"
     table[wettest_key] = (wettest_val, wettest_perc)
 
@@ -181,12 +190,12 @@ def read_run_to_structure_csv(df: pd.DataFrame) -> dict:
 def read_all_runs_to_structure_csv(csv_filename: str) -> dict:
     # Create dataframe from the given file
     df = pd.read_csv(csv_filename, index_col=0, parse_dates=True)
-    
+
     # scenarios we are interested in
     scen_list = ["AdjHist", "CC50", "CC75", "CC95"]
 
     # Read all the scenarios
-    #scen_aliases = df.Scenario.unique()
+    # scen_aliases = df.Scenario.unique()
     combined_struct = dict()
     for scenario in scen_list:
         print("processing scenario: ", scenario)
@@ -205,40 +214,40 @@ def read_all_runs_to_structure_csv(csv_filename: str) -> dict:
 def read_run_to_structure(dss_filename: str) -> dict:
     # Structure to return
     table = {}
-    
+
     # Create DSS object for the given file
     dss_object = pdss.DSS(dss_filename)
 
     # DSS key path for timeseries
     path_swp_list = [
-        "/CALSIM/SWP_TA_TOTAL/SWP_DELIVERY/.*/1MON/L2020A/", 
-        "/CALSIM/SWP_CO_TOTAL/SWP_DELIVERY/.*/1MON/L2020A/", 
-        "/CALSIM/SWP_TA_FEATH/SWP_DELIVERY/.*/1MON/L2020A/", 
-        "/CALSIM/SWP_CO_FEATH/SWP_DELIVERY/.*/1MON/L2020A/"
-        ]
-    
+        "/CALSIM/SWP_TA_TOTAL/SWP_DELIVERY/.*/1MON/L2020A/",
+        "/CALSIM/SWP_CO_TOTAL/SWP_DELIVERY/.*/1MON/L2020A/",
+        "/CALSIM/SWP_TA_FEATH/SWP_DELIVERY/.*/1MON/L2020A/",
+        "/CALSIM/SWP_CO_FEATH/SWP_DELIVERY/.*/1MON/L2020A/",
+    ]
+
     # date range we are interested in
     start = pd.to_datetime("1921-10-01")
     end = pd.to_datetime("2021-09-30")
 
     frames: list[pd.DataFrame] = []
-    
+
     for path_string_swp in path_swp_list:
         # Get the data frame for the given path
         df = table_a(dss_object, path_string_swp, start, end)
 
         # Now add the dataframe to our list of frames
-        frames.append(df)    
+        frames.append(df)
 
     # Now calculate- the timeseries for Table A
     df_A = frames[0] + frames[1]
     df_A = df_A - frames[2]
     df_A = df_A - frames[3]
-    #print(df_A)
+    # print(df_A)
 
     # Convert df from monthly to yearly
     calendar_year_df = df_A.resample(pd.offsets.YearEnd()).sum()
-    
+
     # Column 1. long term average
     table["Long-term Average"] = period_avg(calendar_year_df)
 
@@ -281,17 +290,15 @@ def read_all_runs_to_structure_1(runs: dict[str, str]) -> dict:
     for name, path in runs.items():
         # Dictionary for current table from the current file: key = table_1
         table_1 = {}
-        
+
         # Build the row_1 (Adjusted Historical) of table_1
         structure = read_run_to_structure(path)
 
         # Assign the Adjusted Historical structure to table_1
-        table_1['Adjusted Historical (1922-2021)'] = structure
-
+        table_1["Adjusted Historical (1922-2021)"] = structure
 
         # Calculate and assign the CC 50% structure to table_1
         # TODO: table_1['CC 50% (1922-2021)'] = ...
-
 
         # Calculate and assign the CC 75% structure to table_1
         # TODO: table_1['CC 75% (1922-2021)'] = ...
@@ -311,17 +318,15 @@ def read_all_runs_to_structure(runs: dict[str, str]) -> dict:
     for name, path in runs.items():
         # Dictionary for current table from the current file: key = table_1
         table_1 = {}
-        
+
         # Build the row_1 (Adjusted Historical) of table_1
         structure = read_run_to_structure(path)
 
         # Assign the Adjusted Historical structure to table_1
-        table_1['Adjusted Historical (1922-2021)'] = structure
-
+        table_1["Adjusted Historical (1922-2021)"] = structure
 
         # Calculate and assign the CC 50% structure to table_1
         # TODO: table_1['CC 50% (1922-2021)'] = ...
-
 
         # Calculate and assign the CC 75% structure to table_1
         # TODO: table_1['CC 75% (1922-2021)'] = ...
@@ -349,35 +354,27 @@ if __name__ == "__main__":
     # # DEBUG
     # print_structure(struct)
 
-    # TODO: hand the struct to a frontend 
+    # TODO: hand the struct to a frontend
     # TODO: write the frontend
 
     # pseudo code below
-    #structrure = {
+    # structrure = {
     #    "table_1": {
     #        "dvr_2023_hist":{
     #            (1977, 1977): (wettest_val, wettest_perc)
     #        }
     #    }
-    #}
+    # }
 
-    #value, percent = structrure['table_name']['dssname'][(1977, 1977)]
-    
-    csv_filename = "data\\temp.csv"
-    #start = pd.to_datetime("1921-10-01")
-    #end = pd.to_datetime("2021-09-30")
-    #df = table_a_from_csv(csv_filename, start, end)
-    #print(df.head())
+    # value, percent = structrure['table_name']['dssname'][(1977, 1977)]
+
+    csv_filename = "data\\dv_data.csv"
+    # start = pd.to_datetime("1921-10-01")
+    # end = pd.to_datetime("2021-09-30")
+    # df = table_a_from_csv(csv_filename, start, end)
+    # print(df.head())
 
     result = read_all_runs_to_structure_csv(csv_filename)
     for k, v in result.items():
         print(f"key = '{k}':")
         print(v)
-
-
-
-
-
-
-
-
